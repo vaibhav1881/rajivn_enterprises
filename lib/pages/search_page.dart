969afa9b7f7119310e86c629f4fcd89adddfd6f3
Gaurav <gauravkatheoff@gmail.com>
2024-11-18 20:38:35 +0530

    just nothing

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
-   Route route() => MaterialPageRoute(builder: (_) => const SearchPage());
+  Route route() => MaterialPageRoute(builder: (_) => const SearchPage());
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
   }
 }
-
